2eb1d314500eeb74a9ed290571f5b40bc5c8241e
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-26 13:25:43 +0300

    main small part

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="flex flex-col">
+      <div className="flex gap-3 items-center">
+        <Image className="h-3.5" src="/image/arrowLeft.webp" alt="" width={8} height={14}/>
+        <span className="text-[#76787A] text-sm	">Вернуться к сотрудникам</span>
+      </div>
     </div>
   );
 }
