834decaad5e9b19e23c8aa66aa52c9c5f7a08ef3
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-26 13:05:20 +0300

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="">
-        <img src="" alt="" />
+        <header className="flex justify-between p-5">
+          <Image src="/image/logo.webp" alt="logo" width={180} height={40} />
+          <div className="flex items-center gap-2">
+            <button className="h-10 bg-[#0047BB] px-4 rounded hidden md:block">СОЗДАТЬ СЧЁТ</button>
+            <button className="h-10 bg-[#0047BB] px-4 rounded hidden md:block">КОРЗИНА</button>
+            <button className="flex gap-1 items-center border-solid border-[#363738] border rounded-2xl px-4 py-1.5">
+              <Image src="/image/avatar.webp" alt="avatar" width={24} height={24} />
+              <Image src="/image/arrow.webp" alt="arrow" width={14} height={8} />
+            </button>
+            <Image className="h-3 ml-2 cursor-pointer" src="/image/burgerMenu.webp" alt="Menu" width={18} height={12} />
+          </div>
         </header>
         <main>
           {children}
