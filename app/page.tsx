f6a74e06fc9ad4e7bfcadbf82d7877070f831ac2
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-27 11:03:23 +0300

    added adaptive profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import "./employee.css";
 
 export default function Home() {
   return (
@@ -11,16 +11,30 @@ export default function Home() {
       </div>
       {/* Employee */}
       <section className="bg-[#232324] p-8 rounded-xl">
-        <div className="flex gap-8">
-          <Image className="h-40 w-40" src="/image/avatarBig.webp" alt=""  width={160} height={160}/>
+        <div className="profile-info flex gap-8">
+          <Image className="h-40 w-40" src="/image/avatarBig.webp" alt="" width={160} height={160} />
           <div className="flex justify-center flex-col ">
-            <h2 className="text-5xl my-8">Иванов <pre></pre> Иван Иванович</h2>
-            <div className="flex flex-col gap-4 text-[#E1E3E6] ">
+            <h2 className="profile-name text-5xl my-8">Иванов <pre></pre> Иван Иванович</h2>
+            <div className="profile-post flex flex-col gap-4 text-[#E1E3E6] ">
               <span>Junior UI/UX designer</span>
               <span>Россия, Красноярск <span className="text-[#76787A]">• 14:03</span></span>
             </div>
-
           </div>
+        </div>
+        <nav className=" mt-12 b">
+          <ul className="flex  text-[#B0B1B6] text-sm overflow-x-auto whitespace-nowrap scrollbar-hide">
+            <li className="employee__link ">ОСНОВНАЯ ИНФОРМАЦИЯ</li>
+            <li className="employee__link active">ОТПУСК</li>
+            <li className="employee__link ">ОБОРУДОВАНИЕ</li>
+          </ul>
+        </nav>
+      </section>
+      <section className="vacation">
+        <div>
+          
+        </div>
+        <div>
+
         </div>
       </section>
     </div>
