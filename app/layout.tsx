a3d4e94533cd4d8702a380e89c221672e1ac9f0e
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-27 13:25:45 +0300

    added font

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,12 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <body className={inter.className}>
-        <header className="flex justify-between p-5">
+        <header className="flex justify-between p-5 font-poppins">
           <Image src="/image/logo.webp" alt="logo" width={180} height={40} />
           <div className="flex items-center gap-2">
-            <button className="h-10 bg-[#0047BB] px-4 rounded hidden md:block">СОЗДАТЬ СЧЁТ</button>
-            <button className="h-10 bg-[#0047BB] px-4 rounded hidden md:block">КОРЗИНА</button>
+            <button className="h-10 bg-[#0047BB] px-4 rounded hidden md:block font-semibold">СОЗДАТЬ СЧЁТ</button>
+            <button className="h-10 bg-[#0047BB] px-4 rounded hidden md:block font-semibold">КОРЗИНА</button>
             <button className="flex gap-1 items-center border-solid border-[#363738] border rounded-2xl px-4 py-1.5">
               <Image src="/image/avatar.webp" alt="avatar" width={24} height={24} />
               <Image src="/image/arrow.webp" alt="arrow" width={14} height={8} />
--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,26 @@ import "./employee.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col font-poppins">
       {/* back to List */}
-      <div className="flex gap-3 items-center p-3">
-        <Image className="h-3.5" src="/image/arrowLeft.webp" alt="" width={8} height={14} />
-        <span className="text-[#76787A] text-sm cursor-pointer">Вернуться к сотрудникам</span>
+      <div className="flex gap-3 items-center p-3 ">
+        <Image className="h-3.5 cursor-pointer" src="/image/arrowLeft.webp" alt="" width={8} height={14} />
+        <span className="text-[#76787A] text-sm  font-semibold cursor-pointer">ВЕРНУТЬСЯ К СОТРУДНИКАМ</span>
       </div>
       {/* Employee */}
       <section className="bg-[#232324] p-8 rounded-xl">
         <div className="profile-info flex gap-8">
           <Image className="h-40 w-40" src="/image/avatarBig.webp" alt="" width={160} height={160} />
           <div className="flex justify-center flex-col ">
-            <h2 className="profile-name text-5xl my-8">Иванов <pre></pre> Иван Иванович</h2>
-            <div className="profile-post flex flex-col gap-4 text-[#E1E3E6] ">
+            <h2 className="profile-name text-5xl my-8 font-bold">Иванов <pre></pre> Иван Иванович</h2>
+            <div className="profile-post flex flex-col gap-4 text-[#E1E3E6] font-normal">
               <span>Junior UI/UX designer</span>
               <span>Россия, Красноярск <span className="text-[#76787A]">• 14:03</span></span>
             </div>
           </div>
         </div>
         <nav className=" mt-12 b">
-          <ul className="flex  text-[#B0B1B6] text-sm overflow-x-auto whitespace-nowrap scrollbar-hide">
+          <ul className="flex  text-[#B0B1B6] text-sm overflow-x-auto whitespace-nowrap scrollbar-hide font-semibold">
             <li className="employee__link ">ОСНОВНАЯ ИНФОРМАЦИЯ</li>
             <li className="employee__link active">ОТПУСК</li>
             <li className="employee__link ">ОБОРУДОВАНИЕ</li>
