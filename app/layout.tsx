c968e40f7a045ebd9c044c9e520e5a186dac1a3a
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-27 14:25:37 +0300

    small fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
         <header className="flex justify-between p-5 font-poppins">
           <Image src="/image/logo.webp" alt="logo" width={180} height={40} />
           <div className="flex items-center gap-2">
-            <button className="h-10 bg-[#0047BB] px-4 rounded hidden hover:scale-105 md:block font-semibold">СОЗДАТЬ СЧЁТ</button>
-            <button className="h-10 bg-[#0047BB] px-4 rounded hidden hover:scale-105 md:block font-semibold">КОРЗИНА</button>
+            <button className="h-10 bg-[#0047BB] px-4 rounded hidden  md:block font-semibold">СОЗДАТЬ СЧЁТ</button>
+            <button className="h-10 bg-[#0047BB] px-4 rounded hidden  md:block font-semibold">КОРЗИНА</button>
             <button className="flex gap-1 items-center border-solid border-[#363738] border rounded-2xl px-4 py-1.5">
               <Image src="/image/avatar.webp" alt="avatar" width={24} height={24} />
               <Image src="/image/arrow.webp" alt="arrow" width={14} height={8} />
